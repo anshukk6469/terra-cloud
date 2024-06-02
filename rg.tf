5243a4e5569d0bbe24d47c7cb80f1bf9f4073cf6
anshukk6469 <129054877+anshukk6469@users.noreply.github.com>
2024-06-02 11:21:04 +0530

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -7,3 +7,10 @@ resource "azurerm_resource_group" "example2" {
       name = "deep-new3"
       location = "eastus"
 }
+
+
+resource "azurerm_resource_group" "example3" {
+      name = "deep-new4"
+      location = "eastus"
+}
+
